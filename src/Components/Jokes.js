25d58a1c08b2ff375eaa40975247501d49c4eefe
tinevancorenland <tinevancorenland@gmail.com>
2019-08-29 16:23:11 +0200

    update joke and jokes and styling

--- src/Components/Jokes.js
+++ src/Components/Jokes.js
@@ -4,6 +4,7 @@ import Joke from './Joke'
 function Jokes() {
     return(
         <div className="jokes">
+            <h3 className="joketitle">Procrastinate a little before starting your todo's?</h3>
             <Joke/>
             <Joke/>
             <Joke/>
