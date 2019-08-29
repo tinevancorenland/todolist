11a103685444b46fd33c28d6febe9da0998d1249
tinevancorenland <tinevancorenland@gmail.com>
2019-08-29 12:12:48 +0200

    changed todolist to new component to be able to add other components as well for learning purposes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 // import './App.css';
 
+import Todolist from './Components/Todolist'
+
 function App() {
   return (
-    <div>
-      <input type="checkbox"/>
-      <p>todo one</p>
-      <input type="checkbox"/>
-      <p>todo two</p>
-      <input type="checkbox"/>
-      <p>todo three</p>
-      <input type="checkbox"/>
-      <p>todo four</p>
-    </div>
+    <Todolist/>
   );
 }
 
---
+++ src/Components/Todolist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Todolist() {
+  return (
+    <div>
+      <input type="checkbox"/>
+      <p>todo one</p>
+      <input type="checkbox"/>
+      <p>todo two</p>
+      <input type="checkbox"/>
+      <p>todo three</p>
+      <input type="checkbox"/>
+      <p>todo four</p>
+    </div>
+  );
+}
+
+export default Todolist;
