9bdea009b60ce17cdd7e6b0720886e9e636b70ad
tinevancorenland <tinevancorenland@gmail.com>
2019-08-29 14:50:56 +0200

    change styling

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TodoItem() {
     return(
         <div className="todoitem">
-            <label><input className="checkbox" type="checkbox"/>  Do this thing</label>
+            <label><input type="checkbox"/>   Do this thing</label>
         </div>
     )
 }
