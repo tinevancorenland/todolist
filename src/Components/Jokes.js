5e4b6069acf70fe66226108f291f8e72a82ef6a3
tinevancorenland <tinevancorenland@gmail.com>
2019-08-30 10:12:35 +0200

    create jokeData to practice mapping

--- src/Components/Jokes.js
+++ src/Components/Jokes.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import Joke from './Joke'
+import jokeData from './jokeData'
 
 function Jokes() {
+
+    console.log(jokeData);
+
+    const JokeComponent = jokeData.map(joke =>
+            <Joke key={joke.id} question={joke.question} answer={joke.answer} />)
 
     return(
         <div className="jokes">
             <h3 className="joketitle">Procrastinate a little before starting your todo's?</h3>
-            <Joke 
-                question="Why do bees have sticky hair?" 
-                answer="Because they use honeycombs."
-            />
-            <Joke 
-                question="What do you call a dog with no legs?" 
-                answer="It doesn't matter what you call him, he isn't coming. "
-            />
-            <Joke 
-                question="What's blue and tastes like red paint?" 
-                answer="Blue paint"
-            />
-            <Joke 
-                question="What do you call a rooster staring at a pile of lettuce?" 
-                answer="A chicken sees a salad"
-            />
-            <Joke 
-                question="Thanks for reading this" 
-                answer="nice ass"
-            />
+            {JokeComponent}
         </div>
     )
 }
---
+++ src/Components/jokeData.js
@@ -0,0 +1,29 @@
+const jokeData = [
+    {
+        id:"1",
+        question:"Why do bees have sticky hair?",
+        answer:"Because they use honeycombs."
+    },
+    {
+        id:"2",
+        question:"What do you call a dog with no legs?",
+        answer:"It doesn't matter what you call him, he isn't coming." 
+    },
+    {
+        id:"3",
+        question:"What's blue and tastes like red paint?", 
+        answer:"Blue paint"
+    },
+    {
+        id:"4",
+        question:"What do you call a rooster staring at a pile of lettuce?", 
+        answer:"A chicken sees a salad"
+    },
+    {
+        id:"5",
+        question:"Thanks for reading this", 
+        answer:"Love your shoes"
+    }
+]
+
+export default jokeData
